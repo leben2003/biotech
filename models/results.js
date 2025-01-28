c5bd80dbf8a4c66f1172c6a7ed054bb0370d1d46
leben <ayoubutchiha0@gmail.com>
2025-01-28 14:24:43 -0500

    nsit ila comitit

--- models/results.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const resultSchema = new mongoose.Schema({
-    patientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    type: { type: String, required: true },
-    results: { type: Object, required: true },
-});
-
-module.exports = mongoose.model('Result', resultSchema);
--- routes/resultsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addResult, getResults } = require('../controllers/resultController');
-const verifyToken = require('../middleware/auth');
-const router = express.Router();
-
-router.post('/', verifyToken, addResult);
-router.get('/', verifyToken, getResults);
-
-module.exports = router;
